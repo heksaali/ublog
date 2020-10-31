effbe1cb9815ccb70fb9164382efef5c0a3d2026
Oskari Tiala <oskari.tiala@gmail.com>
2020-10-31 21:45:24 +0200

    Add a simple test scenario

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TestNewPostLogin(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+    def test_posting_form(self):
+        self.client.login(username='temporary', password='temporary')
+        response = self.client.get('/new-post/')
+        self.assertEqual(response.status_code, HTTPStataus.OK)
