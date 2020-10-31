1f331d22fa5db01e05353282dc8a7a056770d826
Oskari Tiala <oskari.tiala@gmail.com>
2020-10-31 21:18:47 +0200

    Allow making new posts from UI

--- ublog/urls.py
+++ ublog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import IndexView, PostDetailView
+from blog.views import IndexView, PostDetailView, NewPostView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
-    path('posts/<slug:slug>/', PostDetailView.as_view(), name='post-view')
+    path('', IndexView.as_view(), name='index-view'),
+    path('posts/<slug:slug>/', PostDetailView.as_view(), name='post-view'),
+    path('new-post/', NewPostView.as_view(), name='new-post-view'),
 ]
